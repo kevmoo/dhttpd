1acee70a486f9ba2bd6ced7a259ef193062f8900
Devon Carew <devoncarew@gmail.com>
2015-08-13 09:42:54 -0700

    move _boot.dart to src/; format code

--- bin/simple_http_server.dart
+++ bin/simple_http_server.dart
@@ -1,6 +1,6 @@
-import '_boot.dart';
+import 'package:simple_http_server/src/_boot.dart';
 
-main(List<String> args) async {
+main(List<String> args) {
   print('Warning: using simple_http_server to start the server is deprecated.');
   print('Please use dhttpd instead. Thanks!');
   bootServer(args);
--- lib/simple_http_server.dart
+++ lib/simple_http_server.dart
@@ -5,31 +5,32 @@ import 'dart:io';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
-import 'package:shelf_static/shelf_static.dart';
 import 'package:shelf_cors/shelf_cors.dart' as shelf_cors;
+import 'package:shelf_static/shelf_static.dart';
 
 const int DEFAULT_PORT = 8080;
 const String DEFAULT_HOST = 'localhost';
 
 class SimpleHttpServer {
   static Future<SimpleHttpServer> start(
-      {String path, int port: DEFAULT_PORT, String allowOrigin, address: DEFAULT_HOST}) async {
+      {String path,
+      int port: DEFAULT_PORT,
+      String allowOrigin,
+      address: DEFAULT_HOST}) async {
     if (path == null) path = Directory.current.path;
 
-    final handler = createStaticHandler(path,
-        defaultDocument: 'index.html');
+    final handler = createStaticHandler(path, defaultDocument: 'index.html');
 
-    final pipeline =
-        const Pipeline().addMiddleware(logRequests());
+    final pipeline = const Pipeline().addMiddleware(logRequests());
 
     if (allowOrigin != null) {
-      final corsHeaders = {
-        'Access-Control-Allow-Origin': allowOrigin,
-      };
-      pipeline.addMiddleware(shelf_cors.createCorsHeadersMiddleware(corsHeaders: corsHeaders));
+      final corsHeaders = {'Access-Control-Allow-Origin': allowOrigin,};
+      pipeline.addMiddleware(
+          shelf_cors.createCorsHeadersMiddleware(corsHeaders: corsHeaders));
     }
 
-    HttpServer server = await io.serve(pipeline.addHandler(handler), address, port);
+    HttpServer server =
+        await io.serve(pipeline.addHandler(handler), address, port);
     return new SimpleHttpServer._(server, path);
   }
 
