185a6da74825ab33fd8221f124d81cf7ac76b987
Kevin Moore <kevmoo@google.com>
2022-01-23 14:43:00 -0800

    Update generation

--- lib/src/options.g.dart
+++ lib/src/options.g.dart
@@ -7,31 +7,51 @@ part of 'options.dart';
 // **************************************************************************
 
 T _$badNumberFormat<T extends num>(
-        String source, String type, String argName) =>
+  String source,
+  String type,
+  String argName,
+) =>
     throw FormatException(
-        'Cannot parse "$source" into `$type` for option "$argName".');
+      'Cannot parse "$source" into `$type` for option "$argName".',
+    );
 
 Options _$parseOptionsResult(ArgResults result) => Options(
-    port: int.tryParse(result['port'] as String) ??
-        _$badNumberFormat(result['port'] as String, 'int', 'port'),
-    path: result['path'] as String?,
-    host: result['host'] as String,
-    help: result['help'] as bool);
+      port: int.tryParse(result['port'] as String) ??
+          _$badNumberFormat(
+            result['port'] as String,
+            'int',
+            'port',
+          ),
+      path: result['path'] as String?,
+      host: result['host'] as String,
+      help: result['help'] as bool,
+    );
 
 ArgParser _$populateOptionsParser(ArgParser parser) => parser
-  ..addOption('port',
-      abbr: 'p',
-      help: 'The port to listen on.',
-      valueHelp: 'port',
-      defaultsTo: '8080')
-  ..addOption('path',
-      help: 'The path to serve. If not set, the current directory is used.',
-      valueHelp: 'path')
-  ..addOption('host',
-      help: 'The hostname to listen on.',
-      valueHelp: 'host',
-      defaultsTo: 'localhost')
-  ..addFlag('help', abbr: 'h', help: 'Displays the help.', negatable: false);
+  ..addOption(
+    'port',
+    abbr: 'p',
+    help: 'The port to listen on.',
+    valueHelp: 'port',
+    defaultsTo: '8080',
+  )
+  ..addOption(
+    'path',
+    help: 'The path to serve. If not set, the current directory is used.',
+    valueHelp: 'path',
+  )
+  ..addOption(
+    'host',
+    help: 'The hostname to listen on.',
+    valueHelp: 'host',
+    defaultsTo: 'localhost',
+  )
+  ..addFlag(
+    'help',
+    abbr: 'h',
+    help: 'Displays the help.',
+    negatable: false,
+  );
 
 final _$parserForOptions = _$populateOptionsParser(ArgParser());
 
