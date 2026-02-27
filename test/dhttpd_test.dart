db34c062caae5238785c4c398574a0cb98f42322
kevmoo <kevmoo@j832.com>
2026-02-27 08:23:50 -0800

    Small tweak

--- test/dhttpd_test.dart
+++ test/dhttpd_test.dart
@@ -76,8 +76,7 @@ void main() {
     expect(server.urlBase, startsWith('https://'));
 
     // Create a client that ignores SSL errors for self-signed certificates
-    final ioClient = HttpClient()
-      ..badCertificateCallback = ((_, _, _) => true);
+    final ioClient = HttpClient()..badCertificateCallback = ((_, _, _) => true);
     final client = http_io.IOClient(ioClient);
 
     try {
